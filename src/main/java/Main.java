13707624e8afc741ec303122aaee8e363e5c21c1
nikitashvets <nvshvets@alfabank.ru>
2024-11-19 00:37:35 +0300

    First homework

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nВведите названия трех гоночных автомобилей и их скорость.\n");
+
+        int countOfRacingCars = 3;
+
+        ArrayList<RacingCar> listRacingCars = raceUtils.getAllRacingCars(countOfRacingCars);
+        raceUtils.getRaceWinner(listRacingCars);
     }
 }
---
+++ src/main/java/RacingCar.java
@@ -0,0 +1,9 @@
+public class RacingCar {
+    String name;
+    int speed;
+
+     RacingCar(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+}
